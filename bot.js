3499a3b98b98ec095e82d8a1f2b29f4ea50ac5de
aqe561nna-ertq252353rbghe-hb-3545ty6-w5 <42757407+aqe561nna-ertq252353rbghe-hb-3545ty6-w5@users.noreply.github.com>
2018-09-12 00:23:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 client.on('ready', function() {
 client.user.setGame('Maintenance | صيانة')
-client.user.setStatus('away', 'Maintenance | صيانة') 
+client.user.setStatus('idle', 'Maintenance | صيانة') 
 console.log(`i am ready ${client.user.username}`);
 });
 
