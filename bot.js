32281577a5fdc48e9e66edd60e3d3baf0151d98a
aqe561nna-ertq252353rbghe-hb-3545ty6-w5 <42757407+aqe561nna-ertq252353rbghe-hb-3545ty6-w5@users.noreply.github.com>
2018-09-12 06:50:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,9 @@ const client = new Discord.Client();
 
 
 client.on('ready', function() {
-client.user.setGame('Maintenance | صيانة')
-client.user.setStatus('idle', 'Maintenance | صيانة') 
+client.user.setGame('nourbot.tk | =help')
 console.log(`i am ready ${client.user.username}`);
 });
 
-//MAINTENANCE
+//MAINTENANCE MUSIC
 client.login(process.env.token)
